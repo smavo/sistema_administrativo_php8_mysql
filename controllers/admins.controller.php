7f02ec63547e1281b916f723a2fedbf77d783ab9
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-01-21 22:23:21 -0500

    Configuracion del CURL PHP para la solicitud a la API

--- controllers/admins.controller.php
+++ controllers/admins.controller.php
@@ -2,7 +2,7 @@
 
 class AdminsController{
 
-    /*===== Login de administradores =====*/
+ 
 	public function login(){
 
 		if(isset($_POST["loginEmail"])){
@@ -10,12 +10,21 @@ class AdminsController{
             /*===== Validamos la sintaxis de los campos =====*/
             if(preg_match('/^[.a-zA-Z0-9_]+([.][.a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST["loginEmail"] )){
                 
-                
+                $url = "users?login=true&suffix=user";
+				$method = "POST";
+				$fields = array(
+
+					"email_user" => $_POST["loginEmail"],
+					"password_user" => $_POST["loginPassword"]
+
+				);
+
+                $response = CurlController::request($url,$method,$fields);
+
+
 
 			}else{
-
 				echo '<div class="alert alert-danger">Error en la sintaxis de campo</div>';
-
 			}
         }
 
