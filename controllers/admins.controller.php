7f5a38ca5bccef0845a73e075deab3b0cf59dc0a
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-01-21 22:07:17 -0500

    Agregando validacion de formulario Login desde el lado del servidor

--- controllers/admins.controller.php
+++ controllers/admins.controller.php
@@ -7,7 +7,16 @@ class AdminsController{
 
 		if(isset($_POST["loginEmail"])){
 
-            
+            /*===== Validamos la sintaxis de los campos =====*/
+            if(preg_match('/^[.a-zA-Z0-9_]+([.][.a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST["loginEmail"] )){
+                
+                
+
+			}else{
+
+				echo '<div class="alert alert-danger">Error en la sintaxis de campo</div>';
+
+			}
         }
 
     }
